bec7bd5d3058ab9d7999b4ed0df80746e66769f8
4jax <dadelos.ddls@gmail.com>
2023-12-17 19:25:41 +0000

    Created a Barrel file for all the screens

---
+++ screens/Screens.js
@@ -0,0 +1,11 @@
+import Lilista from "./Lilista";
+import Magpapalista from "./Magpapalista";
+import Nagpalista from "./Nagpalista";
+import Listahan from "./Listahan";
+
+export default {
+    Lilista,
+    Magpapalista,
+    Nagpalista,
+    Listahan
+};
