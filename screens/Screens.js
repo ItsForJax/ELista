c5325bf32f230f8faa6f6049c1b8ec0daf37750c
4jax <dadelos.ddls@gmail.com>
2023-12-19 03:29:32 +0000

    Added Utang Screen

--- screens/Screens.js
+++ screens/Screens.js
@@ -2,10 +2,12 @@ import Lilista from "./Lilista";
 import Magpapalista from "./Magpapalista";
 import Nagpalista from "./Nagpalista";
 import Listahan from "./Listahan";
+import Utang from "./Utang";
 
 export default {
     Lilista,
     Magpapalista,
     Nagpalista,
-    Listahan
+    Listahan,
+    Utang
 };
