21fed4643dcb3c984bb43f5b3e5b1d3dddf3b39a
4jax <dadelos.ddls@gmail.com>
2023-12-19 03:29:18 +0000

    UI for Add Magpapalista

--- screens/Magpapalista.js
+++ screens/Magpapalista.js
@@ -1,9 +1,42 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { TextInput, SafeAreaView, StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import colors from '../components/colors';
+import globalButton from '../components/button';
 
-export default function Magpapalista() {
+export default function Magpapalista({navigation}) {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Magpapalista Page</Text>
+      <Text style={styles.title}>Magpapalista</Text>
+      <View style={styles.addImage}>
+        <Image  source={require('../assets/PlaceHolder.png')} 
+                resizeMode='contain'
+                style={{width: '95%', height: '95%', alignSelf: 'center', opacity: .5}}/>
+        <Image  source={require('../assets/carbon_add-filled.png')} 
+                resizeMode='contain' 
+                style={{width: 30, 
+                        height: 30, 
+                        position: 'absolute',
+                        right: 10,
+                        bottom: 10,
+              }}/>
+      </View>
+
+      <View style={{}}>
+        <Text style={{width: '90%', alignSelf: 'center', fontWeight: 'bold'}}>Name</Text>
+        <View style={styles.inputName}>
+          <TextInput placeholder='Enter Name' style={{textAlign: 'center'}}/>
+        </View>
+      </View>
+
+      <View style={{flexDirection: 'row', justifyContent: 'center', gap: 20, position: 'absolute', alignSelf: 'center', bottom: 50}}>
+        <TouchableOpacity style={globalButton.button} onPress={() => {
+          navigation.navigate('Listahan')
+          //add snackbar here
+          }}>
+         <Text style={globalButton.buttonText}>ADD PERSON</Text>
+        </TouchableOpacity> 
+      </View>
+      
+      
     </SafeAreaView>
   );
 }
@@ -11,8 +44,31 @@ export default function Magpapalista() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: colors.white,
   },
+  title: {
+    color: colors.text,
+    fontWeight: 'bold',
+    fontSize: 25,
+    textAlign: 'center',
+    marginVertical: 10
+  },
+  addImage: {
+    width: 150,
+    height: 150,
+    padding: 10, 
+    backgroundColor: colors.card,
+    marginBottom: 20,
+    borderRadius: 15,
+    justifyContent: 'center',
+    gap: 10,
+    alignSelf: 'center'
+  },
+  inputName: {
+    backgroundColor: colors.card,
+    width: '90%',
+    padding: 10,
+    alignSelf: 'center',
+    borderRadius: 15,
+  }
 });
